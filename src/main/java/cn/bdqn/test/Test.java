39f7e82670d67b0330385766da03e83675876246
XiaoYan-QAQ <117328977+XiaoYan-QAQ@users.noreply.github.com>
2022-11-03 16:25:38 +0800

    Test

--- src/main/java/cn/bdqn/test/Test.java
+++ src/main/java/cn/bdqn/test/Test.java
@@ -8,10 +8,10 @@ package cn.bdqn.test;
  */
 public class Test {
 
-      public static void main(String [] args){
-      
+    public static void main(String[] args) {
         System.out.println("Test");
-      }
-        
+        System.out.println("123123");
+    }
+
 
 }
